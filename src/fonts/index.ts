a2f13cd9834a278daf0bbc799718525cfbb83603
ellyraad <76833632+ellyraad@users.noreply.github.com>
2025-10-27 08:43:38 +0800

    initial commit

---
+++ src/fonts/index.ts
@@ -0,0 +1,11 @@
+import { Geist, Geist_Mono } from "next/font/google";
+
+export const monaSans = Geist({
+	variable: "--font-mona-sans",
+	subsets: ["latin"],
+});
+
+export const geistMono = Geist_Mono({
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
+});
