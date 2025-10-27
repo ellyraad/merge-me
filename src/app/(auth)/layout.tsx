7e967ca40a3a633428a2a19c5167a51eff674c6d
ellyraad <76833632+ellyraad@users.noreply.github.com>
2025-10-27 18:51:10 +0800

    feat: add register form and page

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,14 @@
+export default function RegisterPage({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<main
+			className="flex min-h-dvh items-center justify-center bg-center bg-cover bg-no-repeat"
+			style={{ backgroundImage: "url('/hero-bg.png')" }}
+		>
+			{children}
+		</main>
+	);
+}
