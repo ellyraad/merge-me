0a4c0a1c4eb3d585d2f278aa80ccbbb0f15493d2
ellyrad <76833632+ellyraad@users.noreply.github.com>
2025-10-27 16:34:03 +0800

    feat(ui): setup heroui component lib (#2)

--- src/app/ui/providers.tsx
+++ src/app/ui/providers.tsx
@@ -1,7 +1,12 @@
 "use client";
 
+import { HeroUIProvider } from "@heroui/react";
 import { ThemeProvider } from "@primer/react-brand";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-	return <ThemeProvider>{children}</ThemeProvider>;
+	return (
+		<ThemeProvider>
+			<HeroUIProvider>{children}</HeroUIProvider>
+		</ThemeProvider>
+	);
 }
