5450b1bebf2f4fd2e0a04e79aef5920eb2814624
ellyraad <76833632+ellyraad@users.noreply.github.com>
2025-10-27 21:55:27 +0800

    feat: implement client-side validation for login form

--- src/app/(auth)/login/components/form.tsx
+++ src/app/(auth)/login/components/form.tsx
@@ -3,10 +3,26 @@
 import { Card, CardBody, CardHeader } from "@heroui/card";
 import { Divider } from "@heroui/divider";
 import { Input } from "@heroui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@primer/react-brand";
+import { type SubmitHandler, useForm } from "react-hook-form";
 import { FaLock } from "react-icons/fa6";
+import { type LoginDataSchema, loginDataSchema } from "@/lib/schemas";
 
 export default function LoginForm() {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		resolver: zodResolver(loginDataSchema),
+		mode: "onTouched",
+	});
+
+	// TODO: implement server action for login first
+	const onSubmit: SubmitHandler<LoginDataSchema> = (data: LoginDataSchema) =>
+		console.log(data);
+
 	return (
 		<Card className="-mt-20 mx-auto w-3/5 md:w-1/3">
 			<CardHeader>
@@ -18,15 +34,26 @@ export default function LoginForm() {
 			<Divider />
 
 			<CardBody className="px-5 py-5">
-				<form className="flex flex-col gap-8">
+				<form className="flex flex-col gap-8" onSubmit={handleSubmit(onSubmit)}>
 					<div className="flex flex-col gap-5">
-						<Input isRequired label="Email" type="email" variant="bordered" />
+						<Input
+							isRequired
+							label="Email"
+							type="email"
+							variant="bordered"
+							isInvalid={!!errors.email}
+							errorMessage={errors.email?.message}
+							{...register("email")}
+						/>
 
 						<Input
 							isRequired
 							variant="bordered"
 							label="Password"
 							type="password"
+							isInvalid={!!errors.email}
+							errorMessage={!!errors.email?.message}
+							{...register("password")}
 						/>
 					</div>
 
