eb151da55a8bbca2dfc37849435347003c16ad24
ellyrad <76833632+ellyraad@users.noreply.github.com>
2025-10-27 18:14:30 +0800

    feat(auth,db): integrate Prisma; setup auth + prisma middleware (#5)
    * feat(db): initialize prisma setup
    
    * feat: load env in prisma config
    
    * feat(auth): integrate Prisma adapter for NextAuth and setup Prisma client
    
    * feat(auth): extract NextAuth config and add middleware for route protection

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,10 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
+import authConfig from "./auth.config";
+import { prisma } from "./lib/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-	providers: [],
+	adapter: PrismaAdapter(prisma),
+	session: { strategy: "jwt" },
+	...authConfig,
 });
