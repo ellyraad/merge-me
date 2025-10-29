934c9c84c0b82b153ff34889c176c5b788609d48
ellyraad <76833632+ellyraad@users.noreply.github.com>
2025-10-29 22:48:58 +0800

    feat: add header and instructions to Discover page

--- src/app/(social)/discover/page.tsx
+++ src/app/(social)/discover/page.tsx
@@ -3,6 +3,14 @@ import { CardStacks } from "@/app/ui/components/swipe-card/swipe-card";
 export default function DiscoverPage() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-center">
+			<div className="w-full text-center text-gray-200/70">
+				<h1 className="font-bold text-lg">Match with your fellow nerds</h1>
+
+				<p className="">
+					Swipe right to approve their pull request, swipe left to close it.
+				</p>
+			</div>
+
 			<CardStacks />
 		</main>
 	);
