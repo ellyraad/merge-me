714919bfda3bb7acb5359bdef2f285eb2055d59d
ellyraad <76833632+ellyraad@users.noreply.github.com>
2025-10-30 13:46:22 +0800

    feat(tooling): add TanStack Query client and provider setup

---
+++ src/lib/queryclient.ts
@@ -0,0 +1,20 @@
+import { isServer, QueryClient } from "@tanstack/react-query";
+
+function makeQueryClient() {
+	return new QueryClient({
+		defaultOptions: {
+			queries: {
+				gcTime: 1000 * 60 * 60,
+			},
+		},
+	});
+}
+
+let browserQueryClient: QueryClient | undefined;
+export function getQueryClient() {
+	if (isServer) {
+		return makeQueryClient();
+	}
+	if (!browserQueryClient) browserQueryClient = makeQueryClient();
+	return browserQueryClient;
+}
