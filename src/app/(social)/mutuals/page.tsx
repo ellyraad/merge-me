62cd4eac623777b5f14b7509917763779ef83ba7
ellyraad <76833632+ellyraad@users.noreply.github.com>
2025-10-31 03:24:33 +0800

    feat(mutuals): redesign mutual list item

--- src/app/(social)/mutuals/page.tsx
+++ src/app/(social)/mutuals/page.tsx
@@ -33,7 +33,7 @@ export default function MutualsPage() {
 				</Tooltip>{" "}
 			</h1>
 
-			<div className="mt-10 flex flex-col gap-4">
+			<div className="mt-10 flex flex-col gap-3">
 				{isLoading &&
 					Array.from({ length: 3 }).map((_, i) => (
 						// biome-ignore lint/suspicious/noArrayIndexKey: skeleton loaders don't change
