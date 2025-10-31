9d398c54f4430a708de6922619bb01d88601911f
ellyraad <76833632+ellyraad@users.noreply.github.com>
2025-10-31 18:51:02 +0800

    fix: bg image not loading properly

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,14 +1,23 @@
+import Image from "next/image";
+import heroBg from "../../../public/hero-bg.png";
+
 export default function RegisterPage({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
 	return (
-		<main
-			className="flex min-h-screen items-center justify-center bg-center bg-cover bg-no-repeat"
-			style={{ backgroundImage: "url('/hero-bg.png')" }}
-		>
-			{children}
+		<main className="relative flex min-h-screen items-center justify-center">
+			<Image
+				src={heroBg}
+				alt="Background"
+				fill
+				priority
+				className="object-cover"
+				quality={90}
+				placeholder="blur"
+			/>
+			<div className="relative z-10">{children}</div>
 		</main>
 	);
 }
--- src/app/ui/components/hero.tsx
+++ src/app/ui/components/hero.tsx
@@ -1,29 +1,39 @@
 "use client";
 
 import { Hero } from "@primer/react-brand";
+import Image from "next/image";
 import { HiHeart } from "react-icons/hi";
+import heroBg from "../../../../public/hero-bg.png";
 
 export default function HeroSection() {
 	return (
-		<div
-			className="min-h-dvh bg-center bg-cover bg-no-repeat"
-			style={{ backgroundImage: "url('/hero-bg.png')" }}
-		>
-			<Hero align="center">
-				<Hero.Heading>
-					<span className="mt-20 flex items-center gap-4 text-6xl md:text-9xl">
-						LGTM <HiHeart className="mb-2 md:mb-6" />
-					</span>
-				</Hero.Heading>
+		<div className="relative min-h-dvh">
+			<Image
+				src={heroBg}
+				alt="Hero background"
+				fill
+				priority
+				className="object-cover"
+				quality={90}
+				placeholder="blur"
+			/>
+			<div className="relative z-10">
+				<Hero align="center">
+					<Hero.Heading>
+						<span className="mt-20 flex items-center gap-4 text-6xl md:text-9xl">
+							LGTM <HiHeart className="mb-2 md:mb-6" />
+						</span>
+					</Hero.Heading>
 
-				<Hero.Description>
-					Submit your <span className="line-through">Love</span> Pull Request
-				</Hero.Description>
+					<Hero.Description>
+						Submit your <span className="line-through">Love</span> Pull Request
+					</Hero.Description>
 
-				<Hero.PrimaryAction variant="accent" size="large" href="/register">
-					Create an account
-				</Hero.PrimaryAction>
-			</Hero>
+					<Hero.PrimaryAction variant="accent" size="large" href="/register">
+						Create an account
+					</Hero.PrimaryAction>
+				</Hero>
+			</div>
 		</div>
 	);
 }
